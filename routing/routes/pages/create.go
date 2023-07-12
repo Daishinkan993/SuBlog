97ad5d99c2dc94d796f1e5e42d53ad068da874f6
Daishinkan993 <121032621+Daishinkan993@users.noreply.github.com>
2023-07-12 23:40:46 +0700

    \\

---
+++ routing/routes/pages/create.go
@@ -0,0 +1,17 @@
+package pages
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func Create(w http.ResponseWriter, r *http.Request) {
+	page, err := os.ReadFile("C:\\SuBlog\\routing\\routes\\pages\\create.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprint(w, string(page))
+}
---
+++ routing/routes/pages/index.go
@@ -0,0 +1,50 @@
+package pages
+
+import (
+	"SuBlog/models"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+)
+
+type PageData struct {
+	Posts []models.Post `json:"Posts"`
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Get("http://127.0.0.1:8080/api/posts")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	var pageData PageData
+	err = json.Unmarshal(body, &pageData)
+	if err != nil {
+		fmt.Println("Error marshall ", err)
+		fmt.Println(resp.Body, string(body))
+		return
+	}
+
+	tmpl, err := template.ParseFiles("C:\\SuBlog\\routing\\routes\\pages\\index.html") // Replace with the path to your HTML template file
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	err = tmpl.Execute(w, pageData)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+}
